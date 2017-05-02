beb13b7e6823a0e7d1ac8787b4769193c36546c6
Hiroaki KARASAWA <siqvare@gmail.com>
2017-05-02 12:16:23 +0900

    Tweak devise view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :omniauthable,
          omniauth_providers: [ :google ]
 
   has_many :attendance_records, dependent: :destroy
