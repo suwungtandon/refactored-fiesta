0df7b4c2820f6537c0ef4f70155ec27630aa9796
Hiroaki KARASAWA <siqvare@gmail.com>
2017-05-02 11:11:58 +0900

    Show flash messages

--- app/controllers/attendance_records_controller.rb
+++ app/controllers/attendance_records_controller.rb
@@ -9,9 +9,11 @@ class AttendanceRecordsController < ApplicationController
     prms = attendance_record_params.merge({ user: current_user })
     prms[:break_time] = parse_time_to_duration(prms[:break_time])
 
-    AttendanceRecord.create(prms)
-
-    # flash note
+    if AttendanceRecord.create(prms)
+      flash[:notice] = 'Created !'
+    else
+      flash[:alert] = 'Failed to create !!'
+    end
 
     redirect_to controller: :index, action: :index
   end
@@ -23,18 +25,24 @@ class AttendanceRecordsController < ApplicationController
   def update
     @attendance_record = AttendanceRecord.find(params[:id])
 
-    @attendance_record.update_attributes!(attendance_record_params)
+    if @attendance_record.update_attributes!(attendance_record_params)
+      flash[:notice] = 'Updated !'
+    else
+      flash[:alert] = 'Failed to update !!'
+    end
 
-    # flash note
     redirect_to controller: :index, action: :index
   end
 
   def destroy
     @attendance_record = AttendanceRecord.find(params[:id])
 
-    @attendance_record.destroy!
+    if @attendance_record.destroy!
+      flash[:notice] = 'Destroyed !'
+    else
+      flash[:alert] = 'Failed to destroy !!'
+    end
 
-    # flash note
     redirect_to controller: :index, action: :index
   end
 
@@ -44,7 +52,7 @@ class AttendanceRecordsController < ApplicationController
   end
 
   def parse_time_to_duration(dtime)
-    ret = dtime.match /^(\d+):(\d+):(\d+)$/
+    ret = dtime.match(/^(\d+):(\d+):(\d+)$/)
 
     raise 'Fucking format' unless ret.size == 4
 
