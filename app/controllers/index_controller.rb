5158140d20bf1ac1c5b0f6e6141178292df03306
Hiroaki KARASAWA <siqvare@gmail.com>
2017-05-02 21:38:37 +0900

    Add welcome page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,10 @@
 class IndexController < ApplicationController
   def index
+    unless current_user.present?
+      render 'welcome', layout: false
+      return
+    end
+
     target_date = params[:target_date] || Time.now
     beginning_of_month = target_date.beginning_of_month
     end_of_month = target_date.end_of_month
