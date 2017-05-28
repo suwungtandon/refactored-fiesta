c347bfe32857d1414941a905ce995d45bde1412d
Hiroaki KARASAWA <siqvare@gmail.com>
2017-05-29 00:47:31 +0900

    Add download csv routing

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
 
   post '/attendance_records/new', to: 'attendance_records#new'
 
+  get '/attendance_records/download', to: 'attendance_records#download'
+
   root 'index#index'
 end
