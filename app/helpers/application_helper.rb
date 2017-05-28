29b0add94d22c471d0af6abb189dcbb04778d725
Hiroaki KARASAWA <siqvare@gmail.com>
2017-05-29 00:47:18 +0900

    Tweak format of the time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def formatted_duration(duration)
-    "%02d:%02d:%02d" % [ duration / 60 / 60, duration / 60 % 60, duration % 60 ]
+    "%02d:%02d" % [ duration / 60 / 60, duration / 60 % 60 ]
   end
 
   def formatted_time_with_break_time(attendance_record)
-    start_time = attendance_record.start_time.strftime('%X')
-    end_time = attendance_record.end_time.strftime('%X')
+    start_time = attendance_record.start_time.strftime('%-H:%M')
+    end_time = attendance_record.end_time.strftime('%-H:%M')
     break_time = formatted_duration(attendance_record.break_time)
 
     "#{start_time} - #{end_time} (#{break_time})"
