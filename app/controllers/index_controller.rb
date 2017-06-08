f40f1e72131cc1564d6bc166da26d392dc31fbc9
Hiroaki KARASAWA <siqvare@gmail.com>
2017-06-08 11:44:08 +0900

    Fix error on visiting new user

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,11 +5,6 @@ class IndexController < ApplicationController
       return
     end
 
-    first_record = AttendanceRecord.where(user: current_user).first.start_time.to_date
-    last_record = AttendanceRecord.where(user: current_user).last.start_time.to_date
-
-    @candidate_months = (first_record .. last_record).map(&:beginning_of_month).uniq
-
     year = (params[:year] || Time.now.year).to_i
     month = (params[:month] || Time.now.month).to_i
 
@@ -19,5 +14,12 @@ class IndexController < ApplicationController
     end_of_month = @target_month.end_of_month
 
     @attendance_records = AttendanceRecord.where(user: current_user, start_time: beginning_of_month .. end_of_month)
+
+    return unless AttendanceRecord.where(user: current_user).present?
+
+    first_record = AttendanceRecord.where(user: current_user).first.start_time.to_date
+    last_record = AttendanceRecord.where(user: current_user).last.start_time.to_date
+
+    @candidate_months = (first_record .. last_record).map(&:beginning_of_month).uniq
   end
 end
